91cd27ac7629412d7b5f928dbfd54b6d09d0c26f
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2024-02-18 23:51:35 +0100

    final commit

--- packages/nextjs/app/create/page.tsx
+++ packages/nextjs/app/create/page.tsx
@@ -1,32 +1,140 @@
 "use client";
 
 import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import deployedContracts from "~~/contracts/deployedContracts";
+import { getDefaultProvider } from 'ethers';
+import { Contract } from 'ethers';
+
+async function getUserData(address: string, abiClosedSea: any) {
+    const provider = getDefaultProvider()
+    const contract = new Contract(address, abiClosedSea, provider);
+    const userData = await contract.getUserData(address);
+    return userData;
+}
+
+async function postImage(selectedFile: any) {
+    // upload file to IPFS
+    const url = "ipfs.io/ipfs/QmRP9FVznm1xxa3FEsDENXhafRDaRWY6LKQdg8GygHNHKw/267.png";
+    return url;
+}
+
+async function postMetadata(metadata: any) {
+    // upload metadata to IPFS
+    const url = "https://ipfs.io/ipfs/QmSaY9zZnKWGa8jmMFNN6LrDGykjSiryUz8YeUjjJ97A8w/267.json";
+    return url;
+}
+
+async function mintNFT(results: any) {
+    results.preventDefault();
+    const file = results.target[0].files[0];
+    const price = results.target[1].value;
+    const trait1Name = results.target[2].value;
+    const trait1Value = results.target[3].value;
+    const trait2Name = results.target[4].value;
+    const trait2Value = results.target[5].value;
+    console.log(file, price, trait1Name, trait1Value, trait2Name, trait2Value);
+    //check if the file is an image
+    if (!file.type.startsWith('image/')) {
+        alert('Please upload an image');
+        return;
+    }
+    // check valid price 
+    if (price <= 0) {
+        alert('Please enter a valid price');
+        return;
+    }
+    // upload image to IPFS with pinada api in .env
+    const url = postImage(file);
+    if (!url) {
+        alert('Error uploading image');
+        return;
+    }
+    // create metadata
+    const metadata = {
+        name: file.name,
+        description: "NFT created with ClosedSea",
+        external_url: url,
+        attributes: [
+            {
+                trait_type: trait1Name,
+                value: trait1Value
+            },
+            {
+                trait_type: trait2Name,
+                value: trait2Value
+            }
+        ]
+    };
+    // upload metadata to IPFS
+    const metadataUrl = postMetadata(metadata);
+    if (!metadataUrl) {
+        alert('Error uploading metadata');
+        return;
+    }
+    // create NFT
+    const { address: connectedAddress } = useAccount();
+    const abiClosedSea = deployedContracts["11155111"]['ClosedSea']['abi'];
+    const addressClosedSea = deployedContracts["11155111"]['ClosedSea']['address'];
+    const provider = getDefaultProvider();
+    const signer = (provider as any).getSigner(connectedAddress);
+    let contract = new Contract(addressClosedSea, abiClosedSea, provider);
+    contract = contract.connect(signer); 
+    const tx = await contract.mintNFT(metadataUrl, price, "2", "$");
+    await tx.wait();
+}
 
 function CreateForm() {
     return (
-        <form className="flex flex-col w-full md:w-1/2">
-            <label className="text-lg font-bold">File</label>
-            <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+        <form onSubmit={mintNFT}>
+            <div className="flex flex-col items-center">
+                <input type="file" className="file-input file-input-bordered file-input-m w-full max-w-xs" required/>
+                <br />
+                <label>Price $</label>
+                <input type="number" placeholder="Amount" className="input input-bordered input-m w-full max-w-xs" required/>
+                <br />
+                <label>Trait 1</label>
+                <input type="text" placeholder="Name" className="input input-bordered input-m w-full max-w-xs" required/>
+                <input type="text" placeholder="Value" className="input input-bordered input-m w-full max-w-xs" required/>
+                <br />
+                <label>Trait 2</label>
+                <input type="text" placeholder="Name" className="input input-bordered input-m w-full max-w-xs" required/>
+                <input type="text" placeholder="Value" className="input input-bordered input-m w-full max-w-xs" required/>
+                <br />
+                <button type="submit" className="btn btn-primary btn-m w-full max-w-xs">Create</button>
+                
+            </div>
         </form>
+        
     );
 }
 
 const Create: NextPage = () => {
 
-    const connectedAddress = getConnectedAddress();
+    const abiClosedSea = deployedContracts["11155111"]['ClosedSea']['abi'];
+    const { address: connectedAddress } = useAccount();
     if (!connectedAddress) return <></>
+    
+    const userData = getUserData(connectedAddress, abiClosedSea);
+    if (!userData) return <></>;
 
-    const userData = getUserData(connectedAddress);
-    if (!userData) return <></>
     const collectionAddress = Object.values(userData)[0];
-    if (collectionAddress === "0x0000000000000000000000000000000000000000") return <></>
+    console.log(collectionAddress);
+    if (collectionAddress === "0x0000000000000000000000000000000000000000") {
+        return (
+            <div className="container mx-auto my-auto">
+                <h1 className="text-4xl font-bold text-center">Create</h1>
+                <br />
+                <p className="text-center">You are not the permission to create NFTs</p>
+            </div>
+        );
+    }
 
     return (
         <div className="container mx-auto my-auto">
-            <h1 className="text-4xl font-bold text-center">Create {collectionAddress}</h1>
-            <div className="container text-center">
-                <CreateForm />
-            </div>
+            <h1 className="text-4xl font-bold text-center">Create</h1>
+            <br />
+            <CreateForm />
         </div>
     );
 };
--- packages/nextjs/app/marketplace/page.tsx
+++ packages/nextjs/app/marketplace/page.tsx
@@ -3,17 +3,54 @@
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { Address } from "~~/components/scaffold-eth";
+import deployedContracts from "~~/contracts/deployedContracts";
+
+function getNFTdata(address: string) {
+  const nftData = {
+    "0xE628Ad5cDDF40B4C338b672c7FC430A0095ae24f": [0],
+    "0x281C38613CB5806090C5f6A37F4256E7e9B659b8": []
+  };
+  return nftData;
+}
+
+
+function getNFTs(address: string) {
+  const nfts = {
+    "0xE628Ad5cDDF40B4C338b672c7FC430A0095ae24f": [0],
+    "0x281C38613CB5806090C5f6A37F4256E7e9B659b8": []
+};
+  return nfts;
+}
+
+function RenderNFTs({ address }: { address: string }) {
+  const abiClosedSea = deployedContracts["11155111"]['ClosedSea']['abi'];
+  const addressClosedSea = deployedContracts["11155111"]['ClosedSea']['address'];
+
+  const nfts: { [key: string]: number[] | never[] } = getNFTs(address);
+  let l = []
+  for (const address in nfts) {
+    for (const tokenId of nfts[address]) {
+      l.push(getNFTdata(address, tokenId));
+
+
+    }
+  }
+  return (
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        
+      </div>
+    );
+}
 
 const Marketplace: NextPage = () => {
   const { address: connectedAddress } = useAccount();
-
+  if (!connectedAddress) return <></>;
   return (
-    <div className="container mx-auto my-auto">
-      <h1 className="text-4xl font-bold text-center my-8">Marketplace</h1>
-      <div className="flex flex-col md:flex-row">
-        
+      <div className="container mx-auto my-auto">
+          <h1 className="text-4xl font-bold text-center">Marketplace</h1>
+          <br />
+          <RenderNFTs address={connectedAddress} />
       </div>
-    </div>
   );
 };
 
